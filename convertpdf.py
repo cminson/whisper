26bec039f4cb68ef08ebeafd09ac92afdc5f705f
cminson <christopherjayminson@gmail.com>
2023-05-01 14:42:48 -0700

    .

---
+++ convertpdf.py
@@ -0,0 +1,53 @@
+#!/usr/local/bin/python
+# 
+# Run locally on Mac to filter input files for whisper
+#
+import json
+import os
+import shutil
+import PyPDF2
+
+PATH_PDF_ARCHIVE = '/Users/Chris/Documents/PDF.ARCHIVE'
+PATH_TRANSCRIPTS = './transcripts'
+
+list_pdfs = os.listdir(PATH_PDF_ARCHIVE)
+for pdf in list_pdfs:
+
+    if '.pdf' not in pdf:
+        print(f'Skipping non-pdf file {pdf}')
+        continue
+    if '.eml' in pdf:
+        print(f'Skipping non-pdf file {pdf}')
+        continue
+
+
+    path_pdf = os.path.join(PATH_PDF_ARCHIVE, pdf)
+    transcript = pdf.replace('.pdf', '.mp3.txt')
+    path_transcript = os.path.join(PATH_TRANSCRIPTS, transcript)
+
+
+    # if no txt transcript for this pdf, read the pdf text and create one
+    if os.path.exists(path_transcript) == False:
+        pdf_file = open(path_pdf, 'rb')
+        print(f'reading pdf {path_pdf}')
+        pdf_reader = PyPDF2.PdfReader(pdf_file)
+        num_pages = len(pdf_reader.pages)
+
+        # Loop through each page and extract the text
+        full_text = ''
+        for page in pdf_reader.pages:
+            # Extract the text from the page
+            text = page.extract_text()
+            full_text += text
+    
+        #print(full_text)
+        # Close the PDF file
+        pdf_file.close()
+
+        print(f'Outputing transcript {path_transcript}')
+        with open(path_transcript, 'w') as fd:
+            fd.write(full_text)
+
+
+
+
---
+++ transcribe.py
@@ -0,0 +1,71 @@
+#!/usr/local/bin/python
+import os
+import sys
+import json
+from os import path
+import subprocess
+
+
+PATH_MP3 = '/Volumes/AudioDharma/AD/TALKS_BACKUP'
+PATH_TRANSCRIPT = './transcripts/'
+CONFIG_JSON = '/Volumes/AudioDharma/AD/CONFIG00.JSON'
+
+COMMAND = '/Library/Frameworks/Python.framework/Versions/3.10/bin/whisper --verbose False  /Volumes/AudioDharma/AD/TALKS_BACKUP/{} -o ./transcripts'
+
+
+ARG_MUST_INCLUDE = ''     # default filter -> accept all mp3s
+if len(sys.argv) > 1:
+    ARG_MUST_INCLUDE = sys.argv[1]
+
+with open(CONFIG_JSON,'r') as fd:
+    list_talks  = json.load(fd)['talks']
+
+for talk in list_talks:
+    url = talk['url']
+    date = talk['date']
+
+    # filters
+    if 'Teacher' in url: continue
+
+    # only convert talk if it has ARG_MUST_INCLUDE in date
+    if ARG_MUST_INCLUDE not in date: 
+        print(f'filtering via {ARG_MUST_INCLUDE}: {url}')
+        continue
+
+    file_mp3 = os.path.basename(url)
+    file_transcript = file_mp3 + '.txt'
+    path_mp3 = os.path.join(PATH_MP3, file_mp3)
+    path_transcript = os.path.join(PATH_TRANSCRIPT, file_transcript)
+    print(path_transcript)
+    path_vtt = path_transcript.replace('.txt', '.vtt')
+    path_srt = path_transcript.replace('.txt', '.srt')
+    print(f'removing {path_vtt}')
+    print(f'removing {path_srt}')
+
+    # don't try to transcribe a non-existent mp3
+    if path.exists(path_mp3) == False:
+        print(f'skipping: {path_mp3}')
+        continue
+
+    #print(path_mp3)
+    #print(path_transcript)
+
+    # if transcript already exists, skip it
+    if path.exists(path_transcript):
+        print(f'skipping: {path_transcript}')
+        continue
+
+    # all good.  form the whisper command and execute
+    command = COMMAND.format(file_mp3)
+    #print(command)
+    print("creating transcript: ", path_transcript)
+    subprocess.run(command, shell=True)
+
+    # clean up the unneeded files
+    path_vtt = path_transcript.replace('.txt', '.vtt')
+    path_srt = path_transcript.replace('.txt', '.srt')
+    print(f'removing {path_vtt}')
+    os.remove(path_vtt)
+    print(f'removing {path_srt}')
+    os.remove(path_srt)
+
